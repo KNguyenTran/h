6897e0337786929429a25efabf62cfac732e18a9
KNguyenTran <trankhoinguyencmu@gmail.com>
2025-06-12 13:19:21 +0700

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,4 @@
+import qrcode
+
+qr = qrcode.make("Chúc mừng bấy bì đã được trúng giải thưởng bên anh trọn đời\n Bấy bì chọn nhận thưởng thì hãy bấm phím 1 gửi vào mess của anh nhá💖 ")
+qr.save("heart_qr.png")
